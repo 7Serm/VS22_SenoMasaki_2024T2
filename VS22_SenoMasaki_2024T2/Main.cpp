9594d6fb774eecd38070fad9977a39a298ed061f
seno0010 <mayamaya521999@gmail.com>
2024-06-22 14:21:05 +0900

    一旦コミット

--- VS22_SenoMasaki_2024T2/Main.cpp
+++ VS22_SenoMasaki_2024T2/Main.cpp
@@ -1,105 +1,248 @@
-﻿# include <Siv3D.hpp> // Siv3D v0.6.14
+﻿# include <Siv3D.hpp>
+
+// 敵の位置をランダムに作成する関数
+Vec2 GenerateEnemy()
+{
+	return RandomVec2({ 50, 750 }, -20);
+}
 
 void Main()
 {
-	// 背景の色を設定する | Set the background color
-	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
+	Scene::SetBackground(ColorF{ 0.1, 0.2, 0.7 });
 
-	// 画像ファイルからテクスチャを作成する | Create a texture from an image file
-	const Texture texture{ U"example/windmill.png" };
+	const Font font{ 30 };
 
-	// 絵文字からテクスチャを作成する | Create a texture from an emoji
-	const Texture emoji{ U"🦖"_emoji };
+	// 自機テクスチャ
+	const Texture playerTexture{ U"🤖"_emoji };
+	// 敵テクスチャ
+	const Texture enemyTexture{ U"👾"_emoji };
 
-	// 太文字のフォントを作成する | Create a bold font with MSDF method
-	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
+	// 自機
+	Vec2 playerPos{ 400, 500 };
+	// 敵
+	Array<Vec2> enemies = { GenerateEnemy() };
 
-	// テキストに含まれる絵文字のためのフォントを作成し、font に追加する | Create a font for emojis in text and add it to font as a fallback
-	const Font emojiFont{ 48, Typeface::ColorEmoji };
-	font.addFallback(emojiFont);
+	// 自機ショット
+	Array<Vec2> playerBullets;
+	// 敵ショット
+	Array<Vec2> enemyBullets;
 
-	// ボタンを押した回数 | Number of button presses
-	int32 count = 0;
+	// 自機のスピード
+	constexpr double playerSpeed = 550.0;
+	// 自機ショットのスピード
+	constexpr double playerBulletSpeed = 500.0;
+	// 敵のスピード
+	constexpr double enemySpeed = 100.0;
+	// 敵ショットのスピード
+	constexpr double enemyBulletSpeed = 300.0;
 
-	// チェックボックスの状態 | Checkbox state
-	bool checked = false;
+	// 敵の発生間隔の初期値（秒）
+	constexpr double initialEnemySpawnTime = 2.0;
+	// 敵の発生間隔（秒）
+	double enemySpawnTime = initialEnemySpawnTime;
+	// 敵の発生の蓄積時間（秒）
+	double enemyAccumulator = 0.0;
 
-	// プレイヤーの移動スピード | Player's movement speed
-	double speed = 200.0;
+	// 自機ショットのクールタイム（秒）
+	constexpr double playerShotCoolTime = 0.1;
+	// 自機ショットのクールタイムタイマー（秒）
+	double playerShotTimer = 0.0;
 
-	// プレイヤーの X 座標 | Player's X position
-	double playerPosX = 400;
+	// 敵ショットのクールタイム（秒）
+	constexpr double enemyShotCoolTime = 0.90;
+	// 敵ショットのクールタイムタイマー（秒）
+	double enemyShotTimer = 0.0;
 
-	// プレイヤーが右を向いているか | Whether player is facing right
-	bool isPlayerFacingRight = true;
+	Effect effect;
+
+	// ハイスコア
+	int32 highScore = 0;
+	// 現在のスコア
+	int32 score = 0;
 
 	while (System::Update())
 	{
-		// テクスチャを描く | Draw the texture
-		texture.draw(20, 20);
+		// ゲームオーバー判定
+		bool gameover = false;
 
-		// テキストを描く | Draw text
-		font(U"Hello, Siv3D!🎮").draw(64, Vec2{ 20, 340 }, ColorF{ 0.2, 0.4, 0.8 });
+		const double deltaTime = Scene::DeltaTime();
+		enemyAccumulator += deltaTime;
+		playerShotTimer = Min((playerShotTimer + deltaTime), playerShotCoolTime);
+		enemyShotTimer += deltaTime;
 
-		// 指定した範囲内にテキストを描く | Draw text within a specified area
-		font(U"Siv3D (シブスリーディー) は、ゲームやアプリを楽しく簡単な C++ コードで開発できるフレームワークです。")
-			.draw(18, Rect{ 20, 430, 480, 200 }, Palette::Black);
-
-		// 長方形を描く | Draw a rectangle
-		Rect{ 540, 20, 80, 80 }.draw();
-
-		// 角丸長方形を描く | Draw a rounded rectangle
-		RoundRect{ 680, 20, 80, 200, 20 }.draw(ColorF{ 0.0, 0.4, 0.6 });
-
-		// 円を描く | Draw a circle
-		Circle{ 580, 180, 40 }.draw(Palette::Seagreen);
-
-		// 矢印を描く | Draw an arrow
-		Line{ 540, 330, 760, 260 }.drawArrow(8, SizeF{ 20, 20 }, ColorF{ 0.4 });
-
-		// 半透明の円を描く | Draw a semi-transparent circle
-		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1.0, 0.0, 0.0, 0.5 });
-
-		// ボタン | Button
-		if (SimpleGUI::Button(U"count: {}"_fmt(count), Vec2{ 520, 370 }, 120, (checked == false)))
+		// 敵の発生
+		while (enemySpawnTime <= enemyAccumulator)
 		{
-			// カウントを増やす | Increase the count
-			++count;
+			enemyAccumulator -= enemySpawnTime;
+			enemySpawnTime = Max(enemySpawnTime * 0.95, 0.3);
+			enemies << GenerateEnemy();
 		}
 
-		// チェックボックス | Checkbox
-		SimpleGUI::CheckBox(checked, U"Lock \U000F033E", Vec2{ 660, 370 }, 120);
+		//-------------------
+		//
+		// 移動
+		//
 
-		// スライダー | Slider
-		SimpleGUI::Slider(U"speed: {:.1f}"_fmt(speed), speed, 100, 400, Vec2{ 520, 420 }, 140, 120);
+		// 自機の移動
+		const Vec2 move = Vec2{ (KeyRight.pressed() - KeyLeft.pressed()), (KeyDown.pressed() - KeyUp.pressed()) }
+		.setLength(deltaTime * playerSpeed * (KeyShift.pressed() ? 0.5 : 1.0));
+		playerPos.moveBy(move).clamp(Scene::Rect());
 
-		// 左キーが押されていたら | If left key is pressed
-		if (KeyLeft.pressed())
+		// 自機ショットの発射
+		if (playerShotCoolTime <= playerShotTimer)
 		{
-			// プレイヤーが左に移動する | Player moves left
-			playerPosX = Max((playerPosX - speed * Scene::DeltaTime()), 60.0);
-			isPlayerFacingRight = false;
+			playerShotTimer -= playerShotCoolTime;
+			playerBullets << playerPos.movedBy(0, -50);
 		}
 
-		// 右キーが押されていたら | If right key is pressed
-		if (KeyRight.pressed())
+		// 自機ショットの移動
+		for (auto& playerBullet : playerBullets)
 		{
-			// プレイヤーが右に移動する | Player moves right
-			playerPosX = Min((playerPosX + speed * Scene::DeltaTime()), 740.0);
-			isPlayerFacingRight = true;
+			playerBullet.y += (deltaTime * -playerBulletSpeed);
+		}
+		// 画面外に出た自機ショットは消滅
+		playerBullets.remove_if([](const Vec2& b) { return (b.y < -40); });
+
+		// 敵の移動
+		for (auto& enemy : enemies)
+		{
+			enemy.y += (deltaTime * enemySpeed);
+		}
+		// 画面外に出た敵は消滅
+		enemies.remove_if([&](const Vec2& e)
+		{
+			if (700 < e.y)
+			{
+				// 敵が画面外に出たらゲームオーバー
+				gameover = true;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		});
+
+		// 敵ショットの発射
+		if (enemyShotCoolTime <= enemyShotTimer)
+		{
+			enemyShotTimer -= enemyShotCoolTime;
+
+			for (const auto& enemy : enemies)
+			{
+				enemyBullets << enemy;
+			}
 		}
 
-		// プレイヤーを描く | Draw the player
-		emoji.scaled(0.75).mirrored(isPlayerFacingRight).drawAt(playerPosX, 540);
+		// 敵ショットの移動
+		for (auto& enemyBullet : enemyBullets)
+		{
+			enemyBullet.y += deltaTime * enemyBulletSpeed;
+		}
+		// 画面外に出た自機ショットは消滅
+		enemyBullets.remove_if([](const Vec2& b) {return (700 < b.y); });
+
+		//-------------------
+		//
+		// 攻撃判定
+		//
+
+		// 敵 vs 自機ショット
+		for (auto itEnemy = enemies.begin(); itEnemy != enemies.end();)
+		{
+			const Circle enemyCircle{ *itEnemy, 40 };
+			bool skip = false;
+
+			for (auto itBullet = playerBullets.begin(); itBullet != playerBullets.end();)
+			{
+				if (enemyCircle.intersects(*itBullet))
+				{
+					// 爆発エフェクトを追加
+					effect.add([pos = *itEnemy](double t)
+					{
+						const double t2 = (1.0 - t);
+						Circle{ pos, 10 + t * 70 }.drawFrame(20 * t2, AlphaF(t2 * 0.5));
+						return (t < 1.0);
+					});
+
+					itEnemy = enemies.erase(itEnemy);
+					playerBullets.erase(itBullet);
+					++score;
+					skip = true;
+					break;
+				}
+
+				++itBullet;
+			}
+
+			if (skip)
+			{
+				continue;
+			}
+
+			++itEnemy;
+		}
+
+		// 敵ショット vs 自機
+		for (const auto& enemyBullet : enemyBullets)
+		{
+			// 敵ショットが playerPos の 20 ピクセル以内に接近したら
+			if (enemyBullet.distanceFrom(playerPos) <= 20)
+			{
+				// ゲームオーバーにする
+				gameover = true;
+				break;
+			}
+		}
+
+		// ゲームオーバーならリセット
+		if (gameover)
+		{
+			playerPos = Vec2{ 400, 500 };
+			enemies.clear();
+			playerBullets.clear();
+			enemyBullets.clear();
+			enemySpawnTime = initialEnemySpawnTime;
+			highScore = Max(highScore, score);
+			score = 0;
+		}
+
+		//-------------------
+		//
+		// 描画
+		//
+
+		// 背景のアニメーション
+		for (auto i : step(12))
+		{
+			const double a = Periodic::Sine0_1(2s, Scene::Time() - (2.0 / 12 * i));
+			Rect{ 0, (i * 50), 800, 50 }.draw(ColorF(1.0, a * 0.2));
+		}
+
+		// 自機の描画
+		playerTexture.resized(80).flipped().drawAt(playerPos);
+
+		// 自機ショットの描画
+		for (const auto& playerBullet : playerBullets)
+		{
+			Circle{ playerBullet, 8 }.draw(Palette::Orange);
+		}
+
+		// 敵の描画
+		for (const auto& enemy : enemies)
+		{
+			enemyTexture.resized(60).drawAt(enemy);
+		}
+
+		// 敵ショットの描画
+		for (const auto& enemyBullet : enemyBullets)
+		{
+			Circle{ enemyBullet, 4 }.draw(Palette::White);
+		}
+
+		effect.update();
+
+		// スコアの描画
+		font(U"{} [{}]"_fmt(score, highScore)).draw(Arg::bottomRight(780, 580));
 	}
 }
-
-//
-// - Debug ビルド: プログラムの最適化を減らす代わりに、エラーやクラッシュ時に詳細な情報を得られます。
-//
-// - Release ビルド: 最大限の最適化でビルドします。
-//
-// - [デバッグ] メニュー → [デバッグの開始] でプログラムを実行すると、[出力] ウィンドウに詳細なログが表示され、エラーの原因を探せます。
-//
-// - Visual Studio を更新した直後は、プログラムのリビルド（[ビルド]メニュー → [ソリューションのリビルド]）が必要な場合があります。
-//
